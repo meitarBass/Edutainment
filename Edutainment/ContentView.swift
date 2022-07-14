fd43635c373f0ad9651e63c5bd2c3c4dfbbbafd9
meitarbasson <meitarbass@gmail.com>
2022-07-14 22:45:33 +0300

    Adding UI

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var maxMultiply = 2
+    @State private var maxQuestion = 5
+    
+    let numOfQuestions = [5, 10, 15 ,20, 25]
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Stepper("Max multiplication", value: $maxMultiply, in:  2...12)
+                .padding(10)
+            Text("Max size is \(maxMultiply)")
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 50, trailing: 10))
+            Section {
+                Text("Number of questions")
+                Picker("Number of questions", selection: $maxQuestion ) {
+                    ForEach(numOfQuestions, id: \.self) {
+                        Text($0, format: .number)
+                    }
+                }.pickerStyle(.segmented)
+            }
+        }
     }
 }
 
